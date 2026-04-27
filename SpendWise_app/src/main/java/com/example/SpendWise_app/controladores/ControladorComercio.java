521aed1bf110af41efdb7ba6f8e7d98dbbcc96e7
Mariana <maritorres3008@gmail.com>
2026-04-26 23:38:53 -0500

    Controladores

--- SpendWise_app/src/main/java/com/example/SpendWise_app/controladores/ControladorComercio.java
+++ SpendWise_app/src/main/java/com/example/SpendWise_app/controladores/ControladorComercio.java
@@ -1,5 +1,32 @@
 package com.example.SpendWise_app.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpendWise_app.modelos.Comercio;
+import com.example.SpendWise_app.servicio.ComercioServicio;
+
+
+
+@RestController
+@RequestMapping("/apineoapp/v1/comercios")
 public class ControladorComercio {
-    
+    @Autowired
+    ComercioServicio servicio;
+
+    public ResponseEntity <?> controladorGuardar(@RequestBody Comercio datos){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.guardar(datos)
+        );
+    }
+
+    public ResponseEntity <?> controladorListar(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.listar_comercios()
+        );
+    }
 }
--- SpendWise_app/src/main/java/com/example/SpendWise_app/controladores/ControladorMedioPago.java
+++ SpendWise_app/src/main/java/com/example/SpendWise_app/controladores/ControladorMedioPago.java
@@ -1,5 +1,32 @@
 package com.example.SpendWise_app.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpendWise_app.modelos.MedioPago;
+import com.example.SpendWise_app.servicio.MedioPagoServicio;
+
+
+
+@RestController
+@RequestMapping("/apineoapp/v1/medioPagos")
 public class ControladorMedioPago {
-    
+    @Autowired
+    MedioPagoServicio servicio;
+
+    public ResponseEntity <?> controladorGuardar(@RequestBody MedioPago datos){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.guardar(datos)
+        );
+    }
+
+    public ResponseEntity <?> controladorListar(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.listar_medio_pago()
+        );
+    }
 }
